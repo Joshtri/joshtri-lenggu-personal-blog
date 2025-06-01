7c208f37c89f1f1c3c8a56998abd0a1d3dac3406
Joshtri <stuffofyos1516@gmail.com>
2025-06-01 14:40:31 +0700

    Add DashboardPage component with placeholder content

---
+++ app/(sys)/sys/dashboard/page.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+export default function DashboardPage() {
+  return (
+    <>
+      <div className="p-6 max-w-4xl mx-auto">
+        <h1 className="text-2xl font-bold mb-4">Bacon Ipsum</h1>
+        Bacon ipsum dolor amet tongue prosciutto beef ribs ground round buffalo
+        cupim filet mignon tri-tip chicken venison burgdoggen capicola. Jowl
+        tongue turkey meatloaf capicola hamburger pork belly shoulder doner
+        shank, chicken ball tip corned beef. Ham spare ribs pork belly pancetta
+        prosciutto. Cow salami picanha, ribeye shank buffalo beef cupim t-bone
+        pastrami jerky hamburger. Alcatra sirloin porchetta brisket filet mignon
+        short loin chuck ham tenderloin rump short ribs salami jerky turducken.
+        T-bone picanha pancetta corned beef brisket, tongue ham hock bacon
+        chislic doner turducken biltong frankfurter fatback.
+        <p className="mb-4"></p>
+        <p className="mb-4">
+          Bresaola chicken tail ball tip turducken, pastrami pork belly salami.
+          Pastrami landjaeger hamburger bacon tri-tip ham hock, bresaola kevin
+          drumstick salami rump. Pancetta kielbasa andouille pig tenderloin.
+          Ribeye flank sausage beef ribs, prosciutto strip steak burgdoggen
+          tongue shank venison jowl. Chislic fatback filet mignon venison, pork
+          chop short loin shoulder cupim chicken spare ribs jerky. Pork loin
+          salami porchetta alcatra biltong chicken. Bacon shankle flank chislic
+          kevin pork belly alcatra pastrami porchetta hamburger jowl.
+        </p>
+        <p className="mb-4">
+          Meatball chislic beef ribs, short ribs andouille frankfurter venison
+          pastrami. Beef shankle shank porchetta. Pastrami salami sirloin
+          meatloaf. Short ribs meatloaf capicola prosciutto alcatra boudin cupim
+          tenderloin bacon shoulder salami ribeye chicken. Pork ground round
+          sausage drumstick andouille pig chuck pancetta. Meatball pork chop
+          hamburger pig beef. Ball tip cupim hamburger filet mignon leberkas
+          spare ribs sirloin tongue capicola rump kevin salami ham hock
+          landjaeger.
+        </p>
+        <p className="mb-4">
+          Turducken cow strip steak t-bone, prosciutto short loin capicola pork
+          flank andouille landjaeger shankle buffalo. T-bone jowl hamburger
+          short ribs brisket drumstick bacon. Filet mignon beef ground round,
+          corned beef pancetta capicola ham doner flank cupim buffalo pork rump
+          swine porchetta. Cupim capicola beef ribs frankfurter biltong sirloin
+          burgdoggen shankle ham hock shank jowl chuck. Pork loin pork chop
+          meatball short ribs meatloaf cow shankle boudin. Salami bacon boudin
+          alcatra andouille pork loin meatball hamburger tenderloin pastrami
+          rump ham shoulder.
+        </p>
+        <p className="mb-4">
+          Shankle leberkas short loin alcatra bacon fatback pastrami ribeye,
+          pork chop bresaola sausage sirloin meatloaf meatball tongue. Ground
+          round buffalo bacon, shank pig ham burgdoggen tongue beef ribs
+          drumstick doner pork loin sirloin. Pastrami turkey beef ribs short
+          loin landjaeger kielbasa. Prosciutto pastrami bresaola ball tip short
+          ribs ham hock cow ribeye shankle rump sausage doner. Bacon boudin
+          tenderloin rump turkey meatball leberkas flank hamburger short loin
+          burgdoggen kevin spare ribs pork belly.
+        </p>
+      </div>
+    </>
+  );
+}
